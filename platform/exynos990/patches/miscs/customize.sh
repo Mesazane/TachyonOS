760abe5cc6c70a23f915c2ad82c441aad8139aa8
Walter Grayz <alenh944@gmail.com>
2026-01-06 19:34:46 +0400

    platform: exynos990: miscs: fix bomb

--- platform/exynos990/patches/miscs/customize.sh
+++ platform/exynos990/patches/miscs/customize.sh
@@ -92,4 +92,5 @@ SET_PROP "product" "bluetooth.profile.vcp.controller.enabled" "false"
 
 if [[ "$TARGET_CODENAME" == "r8s" ]]; then
     ADD_TO_WORK_DIR "r11sxxx" "system" "system/apex/com.android.bt.apex" 0 0 644 "u:object_r:system_file:s0"
+fi
 LOG_STEP_OUT
